1e5d2358c794664ba7382e15f7fc3b501681f2d2
djstephenson7 <dan_0502@hotmail.co.uk>
2018-12-05 13:22:28 +0000

    Refactored menu into array

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,21 +4,29 @@ class Menu
     print 'Welcome. Select your conversion:'
   end
 
+
   def print_menu
-    print '
-    1. cm to inches
-    2. kg to lbs
-    3. Celsius to Fahrenheit
-    4. mm to inches
-    5. meters to feet
-    6. km to miles
-    7. cm to feet
-    8. grams to ounces
-    9. inches to feet
-    10. liters to gallons
-    11. pounds to ounces
-    12. mph to kph
-    13. acres to square feet
-    14. meters to yards'
+    menu = [
+      'cm to inches',
+      'kg to lbs',
+      'Celsius to Fahrenheit',
+      'mm to inches',
+      'meters to feet',
+      'km to miles',
+      'cm to feet',
+      'grams to ounces',
+      'inches to feet',
+      'liters to gallons',
+      'pounds to ounces',
+      'mph to kph',
+      'acres to square feet',
+      'meters to yards'
+    ]
+
+    menu.each_with_index  do |v, i|
+      print i + 1
+      print '. '
+      puts v
+    end
   end
 end
